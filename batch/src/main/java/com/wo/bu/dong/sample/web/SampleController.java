e037041911087432f2d460d0edfc0d4560bd2fba
wobudongjava <w_b_d_java@163.com>
2018-01-27 21:58:40 +0800

    batch module

---
+++ batch/src/main/java/com/wo/bu/dong/sample/web/SampleController.java
@@ -0,0 +1,26 @@
+package com.wo.bu.dong.sample.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wo.bu.dong.sample.service.SampleService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class SampleController {
+    @Autowired
+    private SampleService sampleService;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld() {
+        log.info("helloWorld==> begin");
+        String result = this.sampleService.getHelloMessage();
+        log.info("helloWorld==> end");
+        return result;
+    }
+}
