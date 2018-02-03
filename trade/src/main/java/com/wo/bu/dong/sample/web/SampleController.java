6a37aa67e8bc01f635cf08c115d40d8bcde0fa08
wobudongjava <w_b_d_java@163.com>
2018-02-03 22:44:49 +0800

    application config

--- trade/src/main/java/com/wo/bu/dong/sample/web/SampleController.java
+++ trade/src/main/java/com/wo/bu/dong/sample/web/SampleController.java
@@ -3,14 +3,17 @@ package com.wo.bu.dong.sample.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.wo.bu.dong.sample.dto.SampleDto;
 import com.wo.bu.dong.sample.service.SampleService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
+@RequestMapping("sample")
 public class SampleController {
     @Autowired
     private SampleService sampleService;
@@ -23,4 +26,26 @@ public class SampleController {
         log.info("helloWorld==> end");
         return result;
     }
+
+    @GetMapping("data/null")
+    @ResponseBody
+    public SampleDto sampleTypeNull() {
+        log.info("sampleType==> begin");
+        SampleDto result = new SampleDto();
+        log.info("sampleType, result={}", result);
+        log.info("sampleType==> end");
+        return result;
+    }
+
+    @GetMapping("data/notNull")
+    @ResponseBody
+    public SampleDto sampleType() {
+        log.info("sampleType==> begin");
+        SampleDto result = new SampleDto();
+        result.init();
+        log.info("sampleType, result={}", result);
+        log.info("sampleType==> end");
+        return result;
+    }
+
 }
